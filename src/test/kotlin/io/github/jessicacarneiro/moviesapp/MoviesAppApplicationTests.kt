31efab1cabe95e8a0ecafb35d8b686b01e0fd70b
Jéssica Carneiro <jessicacarneiro@protonmail.com>
2021-06-25 11:54:31 -0300

    Adds initial files for project.

---
+++ src/test/kotlin/io/github/jessicacarneiro/moviesapp/MoviesAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.jessicacarneiro.moviesapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoviesAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

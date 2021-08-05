cf094728512020a72e447481ae7fa6903303e619
Jéssica Carneiro <jessicacarneiro@protonmail.com>
2021-08-05 19:08:54 -0300

    Refactor mongo repository

--- src/main/kotlin/io/github/jessicacarneiro/moviesapp/infrastructure/KMongoRepository.kt
+++ src/main/kotlin/io/github/jessicacarneiro/moviesapp/infrastructure/KMongoRepository.kt
@@ -3,10 +3,12 @@ package io.github.jessicacarneiro.moviesapp.infrastructure
 import com.mongodb.client.FindIterable
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
+import com.mongodb.client.result.DeleteResult
 import com.mongodb.client.result.InsertOneResult
 import io.github.jessicacarneiro.moviesapp.configuration.MongoDBProperties
 import io.github.jessicacarneiro.moviesapp.domain.Movie
 import org.litote.kmongo.KMongo
+import org.litote.kmongo.deleteMany
 import org.litote.kmongo.find
 import org.litote.kmongo.getCollection
 import org.springframework.beans.factory.annotation.Autowired
@@ -15,18 +17,26 @@ import org.springframework.stereotype.Repository
 @Repository
 class KMongoRepository @Autowired constructor(mongoDBProperties: MongoDBProperties) {
 
-    val client = KMongo.createClient()
-    val database: MongoDatabase = client.getDatabase(mongoDBProperties.database.name)
+    private val client = KMongo.createClient()
+    private val database: MongoDatabase = client.getDatabase(mongoDBProperties.database.name)
 
-    fun getMovies(): MongoCollection<Movie> {
-        return database.getCollection()
+    private fun getMoviesCollection(): MongoCollection<Movie> {
+        return database.getCollection<Movie>()
+    }
+
+    fun getMovies(): FindIterable<Movie> {
+        return getMoviesCollection().find()
     }
 
     fun findMovies(query: String): FindIterable<Movie> {
-        return database.getCollection<Movie>().find(query)
+        return getMoviesCollection().find(query)
     }
 
     fun insertMovie(movie: Movie): InsertOneResult {
-        return database.getCollection<Movie>().insertOne(movie)
+        return getMoviesCollection().insertOne(movie)
+    }
+
+    fun deleteMovies(filter: String): DeleteResult {
+        return getMoviesCollection().deleteMany(filter)
     }
 }
--- src/testE2E/kotlin/io/github/jessicacarneiro/moviesapp/KMongoRepositoryTest.kt
+++ src/testE2E/kotlin/io/github/jessicacarneiro/moviesapp/KMongoRepositoryTest.kt
@@ -1,14 +1,11 @@
 package io.github.jessicacarneiro.moviesapp.infrastructure
 
-import com.mongodb.client.MongoClient
-import com.mongodb.client.MongoCollection
-import com.mongodb.client.MongoDatabase
 import io.github.jessicacarneiro.moviesapp.domain.Movie
 import org.amshove.kluent.`should be equal to`
-import org.amshove.kluent.`should be instance of`
 import org.amshove.kluent.`should be`
-import org.junit.jupiter.api.*
-import org.litote.kmongo.deleteMany
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
@@ -22,29 +19,17 @@ class KMongoRepositoryTest {
 
     @BeforeEach
     fun setUp() {
-        repository.getMovies().insertOne(Movie("Blade Runner", 1982, 8.1))
+        repository.insertMovie(Movie("Blade Runner", 1982, 8.1))
     }
 
     @AfterEach
     fun tearDown() {
-        repository.getMovies().deleteMany("{ title: {\$regex : \"Blade Runner.*\"} }")
-    }
-
-    @Test
-    fun `should create client`() {
-        repository.client `should be instance of` MongoClient::class
-    }
-
-    @Test
-    fun `should retrieve database movies-app`() {
-        repository.database `should be instance of` MongoDatabase::class
-        repository.database.name `should be equal to` "movies_app_test"
+        repository.deleteMovies("{ title: {\$regex : \"Blade Runner.*\"} }")
     }
 
     @Test
     fun `should retrieve collection Movies`() {
-        repository.getMovies() `should be instance of` MongoCollection::class
-        repository.getMovies().countDocuments() `should be` 1
+        repository.getMovies().count() `should be` 1
     }
 
     @Test
